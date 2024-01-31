83a4c58e3a8792675135dd2f4d811877fc5860b0
jalal <jalal.alhatamleh@gmail.com>
2024-01-30 23:00:48 -0800

    first

--- app/src/main/java/com/example/tic_toc_app/MainActivity.kt
+++ app/src/main/java/com/example/tic_toc_app/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.tic_toc_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button_WithAI = findViewById<Button>(R.id.button_WithAI)
+        val button_WithFriend = findViewById<Button>(R.id.button_WithFriend)
+
+        button_WithAI.setOnClickListener {
+            val intent = Intent(this, PlayerSecondActivity::class.java)
+            startActivity(intent)
+        }
+
+        button_WithFriend.setOnClickListener {
+            val intent = Intent(this, PlayerSecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
